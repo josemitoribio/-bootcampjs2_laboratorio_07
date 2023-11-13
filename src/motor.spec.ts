e719c8d8b102fe31f6dc2dd5af5e15efc6c80cf4
Josemi Toribio <josemitoribio@gmail.com>
2023-11-13 17:11:00 +0100

    Laboratorio 07 corregido

--- src/motor.spec.ts
+++ src/motor.spec.ts
@@ -8,14 +8,12 @@ import { comprobarEstadoPartida,
         puntosCarta } from "./motor";
 
 describe ("comprobarEstadoPartida", () => {
-    beforeEach(() => {
-    vi.spyOn(partida, "puntos", "get").mockReturnValue(7.5)
-    });
 
     it ("Cuando la puntuación obtenida es superior a 7.5 se pierde la partida", () => {
         // Arrange
         const puntos : number = 8;
         const estadoAComprobar: Estado = "PERDIDO";
+        vi.spyOn(partida, "puntos", "get").mockReturnValue(7.5)
 
         // Act
         const resultado = comprobarEstadoPartida(puntos);
@@ -28,10 +26,62 @@ describe ("comprobarEstadoPartida", () => {
         // Arrange
         const puntos : number = 7.5;
         const estadoAComprobar: Estado = "GANADO";
+        vi.spyOn(partida, "puntos", "get").mockReturnValue(7.5)
 
         // Act
         const resultado = comprobarEstadoPartida(puntos);
 
+        // Assert 
+        expect(resultado).toBe(estadoAComprobar);
+    });
+
+    it ("Debería devolver el estado MENOR_QUE_4 cuando la puntuación sea menor que 4", () => {
+        // Arrange
+        const puntos : number = 3;
+        const estadoAComprobar: Estado = "MENOR_QUE_4";
+        vi.spyOn(partida, "puntos", "get").mockReturnValue(7.5)
+
+        // Act
+        const resultado = comprobarEstadoPartida(puntos);
+
+        // Assert 
+        expect(resultado).toBe(estadoAComprobar);
+    });
+
+    it ("Debería devolver el estado HA_SIDO_5 cuando la puntuación sea igual a 5", () => {
+        // Arrange
+        const puntos : number = 5;
+        const estadoAComprobar: Estado = "HA_SIDO_5";
+        vi.spyOn(partida, "puntos", "get").mockReturnValue(7.5)
+
+        // Act
+        const resultado = comprobarEstadoPartida(puntos);
+
+        // Assert 
+        expect(resultado).toBe(estadoAComprobar);
+    });
+
+    it ("Debería devolver el estado HA_SIDO_6_O_7 cuando la puntuación esté entre 6 y 7", () => {
+        // Arrange
+        const puntos : number = 6.5;
+        const estadoAComprobar: Estado = "HA_SIDO_6_O_7";
+        vi.spyOn(partida, "puntos", "get").mockReturnValue(7.5)
+
+        // Act
+        const resultado = comprobarEstadoPartida(puntos);
+
+        // Assert 
+        expect(resultado).toBe(estadoAComprobar);
+    });
+
+    it("Debería devolver el estado NINGUN_ESTADO cuando no pertenece a ningún estado", () => {
+        // Arrange
+        const puntos: undefined = undefined;
+        const estadoAComprobar: Estado = "NINGUN_ESTADO";
+    
+        // Act
+        const resultado = comprobarEstadoPartida(puntos);
+    
         // Assert 
         expect(resultado).toBe(estadoAComprobar);
     });
@@ -42,10 +92,20 @@ describe ("comprobarEstadoPartida", () => {
 //Función para generar número aleatorio entre 0 y 10.
 
 describe ("numeroAleatorio", () => {
-    it ("Que el número devuelto sea entre 1 y 10", () => {
+    it ("Math random lo formazos a que devuelva 0, debería devolver 1", () => {
+        // Arrange
+        const numeroEsperado: number = 1;
+        vi.spyOn(global.Math, "random").mockReturnValue(0);
+        //Act
+        const resultado: number = numeroAleatorio();
+        //Assert
+        expect(resultado).toBe(numeroEsperado);
+    });
+
+    it ("Math random lo formazos a que devuelva 0.999, debería devolver 10", () => {
         // Arrange
         const numeroEsperado: number = 10;
-        vi.spyOn(global.Math, "floor").mockReturnValue(9);
+        vi.spyOn(global.Math, "random").mockReturnValue(0.999);
         //Act
         const resultado: number = numeroAleatorio();
         //Assert
@@ -54,10 +114,20 @@ describe ("numeroAleatorio", () => {
 });
 
 describe ("numeroCarta", () => {
-    it ("Si la carta sale 9 le sumamos 2 para que sea la carta 11", () => {
+    it ("Si la carta es mayor que 7 le sumamos 2", () => {
         //Arrange
         const numeroEsperado: number = 11;
         const carta: number = 9;
+        //Act
+        const resultado: number = numeroCarta(carta);
+        //Assert
+        expect(resultado).toBe(numeroEsperado);
+    });
+
+    it ("Si la carta es menor que 7 no le sumamos 2", () => {
+        //Arrange
+        const numeroEsperado: number = 7;
+        const carta: number = 7;
         //Act
         const resultado: number = numeroCarta(carta);
         //Assert
@@ -68,10 +138,20 @@ describe ("numeroCarta", () => {
 // Valor de la carta.
 
 describe ("puntosCarta", () => {
-    it ("Forzamos a que si obtenemos la carta 11 el valor sea 0.5", () => {
+    it ("Si la carta es mayor que 7 la puntuación es de 0.5", () => {
         //Arrange
         const puntuacionEsperada: number = 0.5;
         const carta: number = 11
+        //Act
+        const resultado: number = puntosCarta(carta);
+        //Assert
+        expect(resultado).toBe(puntuacionEsperada);
+    });
+
+    it ("Si la carta es menor o igual que 7 la puntuación es su valor real", () => {
+        //Arrange
+        const puntuacionEsperada: number = 4;
+        const carta: number = 4
         //Act
         const resultado: number = puntosCarta(carta);
         //Assert
